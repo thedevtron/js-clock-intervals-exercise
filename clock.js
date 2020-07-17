f8dda99214e2233cab1a7d96172c174d774118d8
Taylor Darneille <tmdarneille@gmail.com>
2020-07-16 22:10:34 -0700

    step 4

--- clock.js
+++ clock.js
@@ -5,3 +5,23 @@ function secondRotation(seconds) {
   var degRotation = 6*seconds
   return degRotation
 }
+
+function minuteRotation(minutes) {
+  // first, convert the minutes to seconds
+  let seconds = 60*minutes
+  // there are 60sec*60min=3600 seconds in a hour
+  // that means, each second needs to rotate the
+  // minutehand by 360/3600=.1 degrees
+  let degRotation = .1*seconds
+  return degRotation
+}
+
+function hourRotation(hours) {
+  // first, convert the hours to minutes
+  let minutes = 60*hours
+  // there are 60min*12hr=720 minutes in a hour
+  // that means, each minute needs to rotate the
+  // minutehand by 360/720=.5 degrees
+  let degRotation = .5*minutes
+  return degRotation
+}
